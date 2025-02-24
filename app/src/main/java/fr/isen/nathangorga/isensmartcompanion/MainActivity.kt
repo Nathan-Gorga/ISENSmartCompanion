1e909061e52434f09a4f24b0bb3a102d6e6aaf79
Nathan Gorga <gorga.nathan05@gmail.com>
2025-02-24 11:44:50 +0100

    UI layout draft : - image - title - input field - button
    Signed-off-by: Nathan Gorga <gorga.nathan05@gmail.com>

--- app/src/main/java/fr/isen/nathangorga/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/nathangorga/isensmartcompanion/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
